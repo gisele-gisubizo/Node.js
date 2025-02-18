6d343e9ad01238cbdcd5ca63a67311534014b7ec
Gisubizo1005200 <gisubizoclasswork@gmail.com>
2025-02-18 17:04:06 +0200

    last changes for day 1 backend

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+
+
+dotenv.config();
+///VARIABLE THAT HOLD VALUES INSIDE THE .ENV FILE
+const port=process.env.PORT ||3000
+const db_user=process.env.DB_USER;
+const db_name=process.env.DB_NAME;
+const db_pass=process.env.DB_PASS;
+
+
+
+// app.get('/getresult',(req,res)=>{
+//     res.send("this is my first project of node js")
+// });
+
+
+// app.post('/postrequest', (req, res) => {
+//     res.send('Got a POST request')
+//   })
+
+// const PORT=5000;
+
+// app.listen(PORT,()=>{
+//     console.log(`the app is running on the port ${PORT}`);
+    
+// })
+
+
+//codes that were copied
+
 const app=express();
 
-app.get('/getresult',(req,res)=>{
-    res.send("this is my first project of node js")
-});
 
-
-app.post('/postrequest', (req, res) => {
-    res.send('Got a POST request')
+const dbUri = `mongodb+srv://${db_user}:${db_pass}@cluster0.el91e.mongodb.net/${db_name}`;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(dbUri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Node API is running on port http://localhost:${port}`);
+     
+    });
   })
-
-const PORT=5000;
-
-app.listen(PORT,()=>{
-    console.log(`the app is running on the port ${PORT}`);
-    
-})
+  .catch((error) => {
+    console.log(error);
+  });
